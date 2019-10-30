063522ae66244a4181dad90555bae7921e02e09b
Shamim Mirzai <sham@mumsnet.com>
2019-10-30 10:57:25 +0000

    formatting and another Closure fix

--- src/MnToolkit/UserSessionsLaravel.php
+++ src/MnToolkit/UserSessionsLaravel.php
@@ -13,21 +13,21 @@ class UserSessionsLaravel
      */
     public function __construct($cookies)
     {
-      $this->cookie_name = 'mnsso';
-      $this->cookie_value_prefix = 'mnsso_';
-      $this->cookies = $cookies;
+        $this->cookie_name = 'mnsso';
+        $this->cookie_value_prefix = 'mnsso_';
+        $this->cookies = $cookies;
 
     }
 
     /**
      * Get User Session from Redis
      *
-     * @param request $request
+     * @param  request  $request
      *
      */
     public function getUserIdFromSession()
     {
-        if(!empty($this->cookies)){
+        if (!empty($this->cookies)) {
             $user = $this->getUserSession($this->cookies);
         }
 
@@ -37,12 +37,12 @@ class UserSessionsLaravel
     /**
      * Get User Session from Redis
      *
-     * @param request $request
+     * @param  request  $request
      *
      */
     public function getUserSession()
     {
-        if(!empty($this->cookies)){
+        if (!empty($this->cookies)) {
 
             $user = Redis::get($this->cookies[$this->cookie_name]);
         }
@@ -53,7 +53,7 @@ class UserSessionsLaravel
     /**
      * Set User Session in Redis
      *
-     * @param request $request
+     * @param  request  $request
      *
      */
     public function setUserSession($user_id, $persistent, $other_attributes = [])
@@ -61,7 +61,7 @@ class UserSessionsLaravel
         $expiry = $persistent ? strtotime("+1 year") : strtotime("+1 day");
 
         $this->cookies[$this->cookie_name] = [
-            'values' => $this->cookie_value_prefix. uniqid(),
+            'values' => $this->cookie_value_prefix.uniqid(),
             'expires' => $expiry,
             'secure' => true,
             'httponly' => true
@@ -74,12 +74,12 @@ class UserSessionsLaravel
     /**
      * Delete User Session from Redis
      *
-     * @param request $request
+     * @param  request  $request
      *
      */
     public function deleteUserSession()
     {
-        if($this->cookies){
+        if ($this->cookies) {
             Redis::del($this->cookies[$this->cookie_name]);
         }
     }
