63559827bc43ee6e0853b73e4bc0c9e4d6600f29
panos <panos@mumsnet.com>
2019-11-21 12:56:05 +0000

    small fix

--- src/MnToolkit/SourceIpLogger.php
+++ src/MnToolkit/SourceIpLogger.php
@@ -15,7 +15,7 @@ class SourceIpLogger
     public function sourceIpLogger($request, Closure $next)
     {
         if ($_SERVER['HTTP_X_FORWARDED_FOR']) {
-            $stringsArray = explode(",", "HTTP_X_FORWARDED_FOR");
+            $stringsArray = explode(",", $_SERVER['HTTP_X_FORWARDED_FOR']);
             $remote_ip = $stringsArray[0] ?? '';
             putenv("REMOTE_ADDR=$remote_ip");
             putenv("HTTP_X_FORWARDED_FOR=$remote_ip");
